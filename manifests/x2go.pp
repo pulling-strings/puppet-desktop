a8f65435614e7dfd432d446ebf3929022dab93ac
ronen <narkisr@gmail.com>
2014-08-16 01:42:02 +0300

    adding client and limiting ssh port using ufw

--- manifests/x2go.pp
+++ manifests/x2go.pp
@@ -1,11 +1,23 @@
 # Setting up http://wiki.x2go.org/doku.php/wiki:repositories:ubuntu
-class desktop::x2go($server=false) {
+class desktop::x2go($server=false, $lan='0.0.0.0') {
+
+  ensure_resource('class', 'apt', {})
+  ensure_resource('class', 'ufw', {})
+
   apt::ppa {'ppa:x2go/stable': }
 
   if($server == true){
     package{['x2goserver', 'x2goserver-xsession']:
       ensure  => present,
       require => Apt::Ppa['ppa:x2go/stable']
+    }
+    ufw::allow { 'ssh port':
+      port => '22',
+      from => $lan,
+    }
+  } else {
+    package{'x2goclient':
+      ensure  => present
     }
   }
 }
