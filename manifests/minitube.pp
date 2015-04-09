f85acc8c4f2e46f3ee7dec87d2d156ce1cbf7107
ronen <narkisr@gmail.com>
2015-04-10 01:43:06 +0300

    adding qt deps

--- manifests/minitube.pp
+++ manifests/minitube.pp
@@ -1,14 +1,23 @@
+# Setting up http://flavio.tordini.org/minitube
 class desktop::minitube {
   $url  = 'http://flavio.tordini.org/files/minitube/'
 
   $deb  = 'minitube64.deb'
 
   exec{'download minitube deb':
-    command     => "wget -P /usr/src/ ${url}/${deb}",
-    environment => ["HOME=${home}"],
-    user        => 'root',
-    path        => ['/usr/bin','/bin'],
-    unless      => "test -f /usr/src/${deb}"
+    command => "wget -P /usr/src/ ${url}/${deb}",
+    user    => 'root',
+    path    => ['/usr/bin','/bin'],
+    unless  => "test -f /usr/src/${deb}"
+  } ->
+
+  package{['libqtgui4', 'libqt4-xml', 'libqt4-network',
+            'libqt4-dbus', 'phonon-backend-vlc']:
+    ensure  => present
+  } ->
+
+  package{['phonon-backend-xine', 'phonon-backend-gstreamer']:
+    ensure  => absent
   } ->
 
   package{'minitube':
@@ -16,5 +25,5 @@ class desktop::minitube {
     source   => "/usr/src/${deb}",
     provider => dpkg
   }
- 
+
 }
