5ffe9d6fd43628cad91d8893dee4bee9437be2f1
narkisr <narkisr@gmail.com>
2015-04-10 03:03:57 +0300

    removing apt

--- manifests/x2go.pp
+++ manifests/x2go.pp
@@ -1,7 +1,6 @@
 # Setting up http://wiki.x2go.org/doku.php/wiki:repositories:ubuntu
 class desktop::x2go($server=false, $lan='0.0.0.0') {
 
-  ensure_resource('class', 'apt', {})
   ensure_resource('class', 'ufw', {})
 
   apt::ppa {'ppa:x2go/stable': }
