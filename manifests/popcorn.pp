71f8dc3e2f98883519f863c85e878c14b77da1b8
narkisr <narkisr@gmail.com>
2015-04-12 02:32:13 +0300

    popcorn requires answer

--- manifests/popcorn.pp
+++ manifests/popcorn.pp
@@ -1,8 +1,10 @@
 # Setting up https://popcorntime.io/
 class desktop::popcorn {
-  apt::ppa {'ppa:webupd8team/popcorntime': } ->
-
-  package{'popcorn-time':
-    ensure  => present
-  }
+  apt::ppa {'ppa:webupd8team/popcorntime': } 
+  
+  # ->
+  #
+  # package{'popcorn-time':
+  #   ensure  => present
+  # }
 }
