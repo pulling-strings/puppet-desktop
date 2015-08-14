b0dc2c8381a285c9088c4b255dc38c2350cf9e84
ronen <narkisr@gmail.com>
2015-08-15 01:04:59 +0300

    adding port and require apt::update class

--- manifests/x2go.pp
+++ manifests/x2go.pp
@@ -1,17 +1,21 @@
 # Setting up http://wiki.x2go.org/doku.php/wiki:repositories:ubuntu
-class desktop::x2go($server=false, $lan='0.0.0.0') {
-
-  ensure_resource('class', 'ufw', {})
+class desktop::x2go(
+  $server=false,
+  $lan='0.0.0.0',
+  $port = '22'
+) {
 
   apt::ppa {'ppa:x2go/stable': }
 
   if($server == true){
+    Class['apt::update'] ->
     package{['x2goserver', 'x2goserver-xsession']:
       ensure  => present,
       require => Apt::Ppa['ppa:x2go/stable']
     }
 
-    ensure_resource('ufw::allow','ssh port', {port => '22', from => $lan})
+    ensure_resource('class', 'ufw', {})
+    ensure_resource('ufw::allow','ssh port', {port => $port, from => $lan})
 
   } else {
     package{'x2goclient':
