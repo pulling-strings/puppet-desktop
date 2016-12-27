75d4f7ab1ca4428bbf7b4d5ee7145419b84ce4e3
narkisr <narkisr@gmail.com>
2016-12-27 22:52:48 +1100

    clearing warninngs

--- manifests/chrome.pp
+++ manifests/chrome.pp
@@ -3,17 +3,19 @@ class desktop::chrome {
 
   if $::operatingsystem == 'Ubuntu' {
     apt::key { 'google-chrome':
-      key        => '7FAC5991',
-      key_source => 'https://dl-ssl.google.com/linux/linux_signing_key.pub'
+      id     => 'EB4C1BFD4F042F6DDDCCEC917721F63BD38B4796',
+      source => 'https://dl-ssl.google.com/linux/linux_signing_key.pub'
     } ->
 
     apt::source{'google-chrome':
+      include      => {
+        src => false,
+        deb => true
+      },
       location     => 'http://dl.google.com/linux/chrome/deb/',
       architecture => 'amd64',
       release      => 'stable',
       repos        => 'main',
-      include_src  => false,
-      include_deb  => true,
     } ->
 
     package{'google-chrome-stable':
